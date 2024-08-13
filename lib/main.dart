54e9adefb31046fb1059fe9d38aed0958d70c9e4
Gaurav Mishra <gauravrajanju@gmail.com>
2024-08-14 01:16:12 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vylee_partner/core/responsive/size_config.dart';
 import 'navigation/navigation.dart';
 import 'navigation/page_routes.dart';
 
 void main() {
+  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
